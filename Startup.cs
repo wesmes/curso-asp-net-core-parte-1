fa07a35e3f8cbf1d0b532faf49f3bb8f732cf7a6
Weslley Gomes da Cruz <w_gomes@protonmail.com>
2023-05-23 23:59:30 -0300

    Módulo 1 - Aula 5: Criando Relatórios de Livros.

--- Startup.cs
+++ Startup.cs
@@ -25,9 +25,21 @@ namespace web
                 app.UseDeveloperExceptionPage();
             }
 
+            var livros = new List<Livro>();
+            livros.Add(new Livro("001","Quem Mexeu na Minha Query?", 12.99m));
+            livros.Add(new Livro("002","Fique Rico com C#", 30.99m));
+            livros.Add(new Livro("003","Java para Baixinhos", 25.99m));
+
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Olá, mundo!");
+
+                foreach(var livro in livros)
+                {
+
+                    await context.Response.WriteAsync($"{livro.Codigo, -10}{livro.Nome, -40}{livro.Preco.ToString("C"), 10}\r\n");
+
+                }
+
             });
         }
     }
